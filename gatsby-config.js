81f8be24fdfc48848986caa0a2fcee2eca5e9d01
IfSince <leonlaade@gmx.de>
2024-01-05 14:11:23 +0100

    remove initial site content, add some site metadata in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,13 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `csr-vision-v2`,
+    title: 'csr-vision-v2',
+    description: 'csr vision hilft Unternehmen dabei, CSR als Chance zu begreifen. Durch Reduzierung der Komplexität, Kuratieren und crossmediale Aufbereitung verwandeln wir CSR-Berichte, die sonst nur von Analysten und Stakeholdern gelesen werden, in echtes Storytelling, das auch ihre Community und Kunden begeistern wird.',
+    author: 'Leon Laade',
     siteUrl: `https://csr.vision`,
+    keywords: 'Design, Nachhaltigkeit, Komplexitätsreduktion',
   },
+  jsxRuntime: 'automatic',
   plugins: [
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
